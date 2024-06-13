ab36505862428cfbf8be54973fe8b5f861ec4b96
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2024-06-13 16:02:12 +0100

    put icon toggle feature

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -4,4 +4,11 @@ const navMenu = document.querySelector('.nav--items');
 menuBtn.addEventListener('click', (e) => {
     e.stopPropagation();
     navMenu.classList.toggle('active');
+    let btnIcon = menuBtn.querySelector('img');
+    if (navMenu.classList.contains('active')) {
+        btnIcon.setAttribute('src', './assets/images/icon-close.svg');
+    }
+    else {
+        btnIcon.setAttribute('src', './assets/images/icon-hamburger.svg');
+    }
 });
--- src/app/scripts/app.ts
+++ src/app/scripts/app.ts
@@ -5,4 +5,12 @@ menuBtn.addEventListener('click', (e) => {
   e.stopPropagation();
 
   navMenu.classList.toggle('active');
+
+  let btnIcon = menuBtn.querySelector('img') as HTMLImageElement;
+  
+  if (navMenu.classList.contains('active')) {
+    btnIcon.setAttribute('src', './assets/images/icon-close.svg');
+  } else {
+    btnIcon.setAttribute('src', './assets/images/icon-hamburger.svg');
+  }
 });
